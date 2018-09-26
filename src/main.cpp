d1f3bded07cd4e010e17e14390308d5d5e37ddeb
David Kendall <david.kendall@northumbria.ac.uk>
2018-09-26 20:57:19 +0100

    Solution for exercise 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include "mbed.h"
  
 DigitalOut led1(LED1);
- 
+DigitalOut led_app_red(D5);
+DigitalOut led_app_green(D9);
+
 int main() {
+    led_app_red = 1;
     while (true) {
         led1 = !led1;
+        led_app_green = !led_app_green;
         wait(0.5);
     }
 }
